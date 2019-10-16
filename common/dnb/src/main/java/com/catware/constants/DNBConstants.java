45655dd7e296a9758731cfefae0222579322ddad
Roar Thronæs <rroart@gmail.com>
2019-10-16 13:11:54 +0200

    More constants.

---
+++ common/dnb/src/main/java/com/catware/constants/DNBConstants.java
@@ -0,0 +1,19 @@
+package com.catware.constants;
+
+public class DNBConstants {
+
+	public static final String PSD2ENDPOINT = "https://sandboxapi.psd.dnb.no/";
+
+	public static final String PSUID = "PSU-ID";
+
+	public static final String TPPREDIRECTURI = "TPP-Redirect-URI";
+
+	public static final String CONSENTID = "Consent-ID";
+
+	public static final String CONTENTTYPE = "Content-Type";
+
+	public static final String ACCEPT = "Accept";
+
+	public static final String XREQUESTID = "X-Request-ID";
+
+}
