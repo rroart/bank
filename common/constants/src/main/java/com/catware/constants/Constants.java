1c2a9713d732051682f9ecaa4f0fd807c789295e
Roar Thronæs <rroart@gmail.com>
2019-10-16 13:10:37 +0200

    More constants.

--- common/constants/src/main/java/com/catware/constants/Constants.java
+++ common/constants/src/main/java/com/catware/constants/Constants.java
@@ -1,5 +1,9 @@
 package com.catware.constants;
 
 public class Constants {
+	public static final String GET = "get";
+	
+	public static final String POST = "post";
+
 	public static final String EXCEPTION = "Exception";
 }
