1caa444a4d383f8ad1511ade0b3016c21c4f2914
Roar Thronæs <rroart@gmail.com>
2019-10-16 13:11:47 +0200

    Request wrapper. More constants. More JSON handling.

--- common/dnb/src/main/java/com/catware/account/impl/AccountServiceImpl.java
+++ common/dnb/src/main/java/com/catware/account/impl/AccountServiceImpl.java
@@ -14,43 +14,44 @@ import java.util.UUID;
 
 import com.catware.account.AccountService;
 import com.catware.constants.Constants;
-import com.catware.util.http.MyRequest;
+import com.catware.constants.DNBConstants;
+import com.catware.util.http.dnb.DNBRequest;
 
 public class AccountServiceImpl extends AccountService {
 	
 	@Override
 	public String getAccount(String consentid) throws UnrecoverableKeyException, KeyManagementException, KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
 		Map<String, String> header = new LinkedHashMap<>();
-		header.put("Consent-ID", consentid);
-		header.put("TPP-Redirect-URI", "http://0.0.0.0:3083");
-		return new MyRequest(Constants.psd2endpoint, "v1/accounts", "GET", header, null).request();
+		header.put(DNBConstants.CONSENTID, consentid);
+		header.put(DNBConstants.TPPREDIRECTURI, "http://0.0.0.0:3083");
+		return new DNBRequest(DNBConstants.PSD2ENDPOINT, "v1/accounts", Constants.GET, header, null).request();
 	}
 	
 	@Override
 	public String getAccountDetails(String consentid, String accid) throws UnrecoverableKeyException, KeyManagementException, KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
 		String requestId = UUID.randomUUID().toString();
 		Map<String, String> header = new LinkedHashMap<>();
-		header.put("Content-Type", "application/json; utf-8");
-		header.put("Accept", "application/json");
-		header.put("X-Request-ID", requestId);
-		header.put("Consent-ID", consentid);
-		header.put("TPP-Redirect-URI", "http://0.0.0.0:3083");
-		return new MyRequest(Constants.psd2endpoint, "v1/accounts/" + accid, "GET", header, null).request();
+		header.put(DNBConstants.CONTENTTYPE, "application/json; utf-8");
+		header.put(DNBConstants.ACCEPT, "application/json");
+		header.put(DNBConstants.XREQUESTID, requestId);
+		header.put(DNBConstants.CONSENTID, consentid);
+		header.put(DNBConstants.TPPREDIRECTURI, "http://0.0.0.0:3083");
+		return new DNBRequest(DNBConstants.PSD2ENDPOINT, "v1/accounts/" + accid, Constants.GET, header, null).request();
 	}
 	
 	@Override
 	public String getBalance(String consentid, String accid) throws UnrecoverableKeyException, KeyManagementException, KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
 		Map<String, String> header = new LinkedHashMap<>();
-		header.put("Consent-ID", consentid);		
-		return new MyRequest(Constants.psd2endpoint, "v1/accounts/" + accid + "/balances", "GET", header, null).request();
+		header.put(DNBConstants.CONSENTID, consentid);		
+		return new DNBRequest(DNBConstants.PSD2ENDPOINT, "v1/accounts/" + accid + "/balances", Constants.GET, header, null).request();
 	}
 
 	@Override
 	public String getAccountTransactions(String consentid, String accid) throws UnrecoverableKeyException, KeyManagementException, KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
 		Map<String, String> header = new LinkedHashMap<>();
-		header.put("Consent-ID", consentid);
-		header.put("TPP-Redirect-URI", "http://0.0.0.0:3083");
-		return new MyRequest(Constants.psd2endpoint, "v1/accounts/" + accid + "/transactions", "GET", header, null).request();
+		header.put(DNBConstants.CONSENTID, consentid);
+		header.put(DNBConstants.TPPREDIRECTURI, "http://0.0.0.0:3083");
+		return new DNBRequest(DNBConstants.PSD2ENDPOINT, "v1/accounts/" + accid + "/transactions", Constants.GET, header, null).request();
 	}
 	
 }
