0bf25e3f4b20da1e086edc6aa1448ffe66b5913c
Roar Thronæs <rroart@gmail.com>
2019-10-16 13:14:57 +0200

    Request wrapper. More constants. More JSON handling.

--- common/dnb/src/main/java/com/catware/consent/impl/ConsentServiceImpl.java
+++ common/dnb/src/main/java/com/catware/consent/impl/ConsentServiceImpl.java
@@ -7,6 +7,7 @@ import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -14,39 +15,56 @@ import java.util.UUID;
 
 import com.catware.consent.ConsentService;
 import com.catware.constants.Constants;
-import com.catware.util.http.MyRequest;
+import com.catware.constants.DNBConstants;
+import com.catware.model.ConsentAccess;
+import com.catware.model.CreateConsent;
+import com.catware.model.CreateConsentResponse;
+import com.catware.util.http.dnb.DNBRequest;
+import com.catware.util.json.JsonUtil;
 
 public class ConsentServiceImpl extends ConsentService {
 
 	@Override
 	public String create(String ssn) throws UnrecoverableKeyException, KeyManagementException, KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
 		Map<String, String> header = new LinkedHashMap<>();
-		header.put("PSU-ID", ssn);
-		header.put("TPP-Redirect-URI", "http://0.0.0.0:3083");
+		header.put(DNBConstants.PSUID, ssn);
+		header.put(DNBConstants.TPPREDIRECTURI, "http://0.0.0.0:3083");
 		String body = getBody(60);
-		return new MyRequest(Constants.psd2endpoint, "v1/consents", "POST", header, body).request();
+		String json = new DNBRequest(DNBConstants.PSD2ENDPOINT, "v1/consents", Constants.POST, header, body).request();
+		CreateConsentResponse createConstentResponse = JsonUtil.convert(json, CreateConsentResponse.class);
+		return createConstentResponse.getConsentId();
 	}
 	
 	@Override
 	public String get(String consentid, String ssn) throws UnrecoverableKeyException, KeyManagementException, KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
 		Map<String, String> header = new LinkedHashMap<>();
-		header.put("PSU-ID", ssn);
-		header.put("TPP-Redirect-URI", "http://0.0.0.0:3083");
-		return new MyRequest(Constants.psd2endpoint, "v1/consents/" + consentid, "GET", header, null).request();
+		header.put(DNBConstants.PSUID, ssn);
+		header.put(DNBConstants.TPPREDIRECTURI, "http://0.0.0.0:3083");
+		return new DNBRequest(DNBConstants.PSD2ENDPOINT, "v1/consents/" + consentid, Constants.GET, header, null).request();
 	}
 	
 	@Override
 	public String getStatus(String consentid, String ssn) throws UnrecoverableKeyException, KeyManagementException, KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
 		Map<String, String> header = new LinkedHashMap<>();
-		header.put("PSU-ID", ssn);
-		header.put("TPP-Redirect-URI", "http://0.0.0.0:3083");
-		return new MyRequest(Constants.psd2endpoint, "v1/consents/" + consentid + "/status", "GET", header, null).request();
+		header.put(DNBConstants.PSUID, ssn);
+		header.put(DNBConstants.TPPREDIRECTURI, "http://0.0.0.0:3083");
+		return new DNBRequest(DNBConstants.PSD2ENDPOINT, "v1/consents/" + consentid + "/status", Constants.GET, header, null).request();
 	}
 	
 	public String getBody(int validDays) {
 		LocalDate date = LocalDate.now();
 		date = date.plusDays(validDays);
 		String dateString = date.getYear() + "-" + myformat(date.getMonthValue()) + "-" + myformat(date.getDayOfMonth());
+		ConsentAccess consentAccess = new ConsentAccess();
+		consentAccess.setAccounts(new ArrayList<>());
+		consentAccess.setBalances(new ArrayList<>());
+		consentAccess.setTransactions(new ArrayList<>());
+		CreateConsent createConsent = new CreateConsent();
+		createConsent.setAccess(consentAccess);
+		createConsent.setCombinedServiceIndicator(false);
+		createConsent.setFrequencyPerDay(3);
+		createConsent.setRecurringIndicator(true);
+		createConsent.setValidUntil(date);
 		String jsonInputString = "{\n" + 
 				"    \"validUntil\": \"" + dateString + "\",\n" + 
 				"    \"frequencyPerDay\": 3,\n" + 
@@ -58,6 +76,8 @@ public class ConsentServiceImpl extends ConsentService {
 				"    \"recurringIndicator\": true,\n" + 
 				"    \"combinedServiceIndicator\": false\n" + 
 				"}";
+		jsonInputString = JsonUtil.convert(createConsent);
+		System.out.println(jsonInputString);
 		return jsonInputString;
 	}
 
