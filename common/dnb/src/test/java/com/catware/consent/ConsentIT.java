e40cebfd1a041b7278bf0a9eab54aab6cf9ea288
Roar Thronæs <rroart@gmail.com>
2019-10-23 20:39:18 +0200

    Move consentId out of client and into db.

--- common/dnb/src/test/java/com/catware/consent/ConsentIT.java
+++ common/dnb/src/test/java/com/catware/consent/ConsentIT.java
@@ -5,12 +5,18 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import com.catware.consent.impl.ConsentServiceImpl;
+import com.catware.util.http.MyResponse;
 
 public class ConsentIT {
-    @Test
+
+	private static final String ssn = "31125464346";
+
+	private static final String ssnerror = "31125464347";
+
+	@Test
     public void test() throws Exception {
-    	String txt = new ConsentServiceImpl().create("31125464346");
-    	System.out.println(txt);
+    	MyResponse response = new ConsentServiceImpl().create(ssn);
+    	System.out.println(response);
     }
 }
 
