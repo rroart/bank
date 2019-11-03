320e1f501b705ee25f7da2a0b32b15db871b4640
Roar Thronæs <rroart@gmail.com>
2019-11-03 21:01:03 +0100

    New class.

--- common/dnb/src/main/java/com/catware/model/ConsentAccount.java
+++ common/dnb/src/main/java/com/catware/model/ConsentAccount.java
@@ -2,7 +2,27 @@ package com.catware.model;
 
 public class ConsentAccount {
 
-	  private String bban = null;
-	  
-	  private String iban = null;
+	private String bban = null;
+
+	private String iban = null;
+
+	public ConsentAccount() {
+		super();
+	}
+
+	public String getBban() {
+		return bban;
+	}
+
+	public void setBban(String bban) {
+		this.bban = bban;
+	}
+
+	public String getIban() {
+		return iban;
+	}
+
+	public void setIban(String iban) {
+		this.iban = iban;
+	}
 }
