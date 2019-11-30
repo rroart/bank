c0a78c146db306e3e0e87b627a9dcce76fa41b7d
Roar Thronæs <rroart@gmail.com>
2019-11-30 22:50:41 +0100

    Revert some changes and start again.

--- terraform/aws/modules/eks/main.tf
+++ terraform/aws/modules/eks/main.tf
@@ -1,51 +1,51 @@
 data "aws_availability_zones" "available" {
-}
+ }
 
-resource "aws_vpc" "demo" {
-cidr_block = "10.0.0.0/16"
+ resource "aws_vpc" "demo" {
+   cidr_block = "10.0.0.0/16"
 
-tags = {
-"Name"                                      = "terraform-eks-demo-node"
-"kubernetes.io/cluster/var.cluster-name" = "shared"
-}
-}
+   tags = {
+     "Name"                                      = "terraform-eks-demo-node"
+     "kubernetes.io/cluster/${var.cluster-name}" = "shared"
+   }
+ }
 
-resource "aws_subnet" "demo" {
-count = 2
+ resource "aws_subnet" "demo" {
+   count = 2
 
-availability_zone = data.aws_availability_zones.available.names[count.index]
-cidr_block        = "10.0.count.index.0/24"
-vpc_id            = aws_vpc.demo.id
+   availability_zone = data.aws_availability_zones.available.names[count.index]
+   cidr_block        = "10.0.${count.index}.0/24"
+   vpc_id            = aws_vpc.demo.id
 
-tags = {
-"Name"                                      = "terraform-eks-demo-node"
-"kubernetes.io/cluster/var.cluster-name" = "shared"
-}
-}
+   tags = {
+     "Name"                                      = "terraform-eks-demo-node"
+     "kubernetes.io/cluster/${var.cluster-name}" = "shared"
+   }
+ }
 
-resource "aws_internet_gateway" "demo" {
-vpc_id = aws_vpc.demo.id
+ resource "aws_internet_gateway" "demo" {
+   vpc_id = aws_vpc.demo.id
 
-tags = {
-Name = "terraform-eks-demo"
-}
-}
+   tags = {
+     Name = "terraform-eks-demo"
+   }
+ }
 
-resource "aws_route_table" "demo" {
-vpc_id = aws_vpc.demo.id
+ resource "aws_route_table" "demo" {
+   vpc_id = aws_vpc.demo.id
 
-route {
-cidr_block = "0.0.0.0/0"
-gateway_id = aws_internet_gateway.demo.id
-}
-}
+   route {
+     cidr_block = "0.0.0.0/0"
+     gateway_id = aws_internet_gateway.demo.id
+   }
+ }
 
-resource "aws_route_table_association" "demo" {
-count = 2
+ resource "aws_route_table_association" "demo" {
+   count = 2
 
-subnet_id      = aws_subnet.demo[count.index].id
-route_table_id = aws_route_table.demo.id
-}
+   subnet_id      = aws_subnet.demo[count.index].id
+   route_table_id = aws_route_table.demo.id
+ }
 
 resource "aws_iam_role" "demo-node" {
   name = "terraform-eks-demo-cluster"
@@ -108,11 +108,11 @@ resource "aws_security_group_rule" "demo-cluster-ingress-workstation-https" {
 
 resource "aws_eks_cluster" "demo" {
   name            = "var.cluster-name"
-  role_arn        = aws_iam_role.demo-node.arn
+  role_arn        = "${aws_iam_role.demo-node.arn}"
 
   vpc_config {
-    security_group_ids = ["aws_security_group.demo-cluster.id"]
-    subnet_ids         = ["aws_subnet.demo.*.id"]
+    security_group_ids = ["${aws_security_group.demo-cluster.id}"]
+    subnet_ids         = flatten(["${aws_subnet.demo.*.id}"])
   }
 
   depends_on = [
@@ -138,22 +138,22 @@ data "aws_iam_policy_document" "kubectl_assume_role_policy" {
 
 resource "aws_iam_role" "eks_kubectl_role" {
   name               = "example-kubectl-access-role"
-  assume_role_policy = data.aws_iam_policy_document.kubectl_assume_role_policy.json
+  assume_role_policy = "${data.aws_iam_policy_document.kubectl_assume_role_policy.json}"
 }
 
 resource "aws_iam_role_policy_attachment" "eks_kubectl-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = "aws_iam_role.eks_kubectl_role.name"
+  role       = "${aws_iam_role.eks_kubectl_role.name}"
 }
 
 resource "aws_iam_role_policy_attachment" "eks_kubectl-AmazonEKSServicePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = "aws_iam_role.eks_kubectl_role.name"
+  role       = "${aws_iam_role.eks_kubectl_role.name}"
 }
 
 resource "aws_iam_role_policy_attachment" "eks_kubectl-AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = "aws_iam_role.eks_kubectl_role.name"
+  role       = "${aws_iam_role.eks_kubectl_role.name}"
 }
 
 # for automating inside terraform
@@ -163,9 +163,9 @@ data "aws_eks_cluster_auth" "cluster_auth" {
 }
 
 provider "kubernetes" {
-  host                   = "aws_eks_cluster.demo.endpoint"
-  cluster_ca_certificate = "base64decode(aws_eks_cluster.demo.certificate_authority.0.data)"
-  token                  = "data.aws_eks_cluster_auth.cluster_auth.token"
+  host                   = "${aws_eks_cluster.my_cluster.endpoint}"
+  cluster_ca_certificate = "${base64decode(aws_eks_cluster.my_cluster.certificate_authority.0.data)}"
+  token                  = "${data.aws_eks_cluster_auth.cluster_auth.token}"
   load_config_file       = false
 }
 
