20790273f76e3a473a655428caa21ed5ac460b72
Roar Thronæs <rroart@gmail.com>
2019-11-30 22:49:53 +0100

    Set variables.

--- terraform/aws/modules/ecs/variables.tf
+++ terraform/aws/modules/ecs/variables.tf
@@ -34,3 +34,22 @@ variable "secret_key_base" {
   description = "The secret key base to use in the app"
 }
 
+variable "fargate_cpu" {
+  description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
+  default     = "256"
+}
+
+variable "fargate_memory" {
+  description = "Fargate instance memory to provision (in MiB)"
+  default     = "512"
+}
+
+variable "aws_region" {
+  description = "The AWS region things are created in"
+  default     = "us-east-1"
+}
+
+variable "app_port" {
+  description = "Port exposed by the docker image to redirect traffic to"
+  default     = 3000
+}
