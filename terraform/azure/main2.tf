31cf00350c7d98aeabb5fc7999fcc2cc9fb9051f
Roar Thronæs <rroart@gmail.com>
2020-01-15 13:35:44 +0100

    Rename. Add Azure pipeline build.

---
+++ terraform/azure/main2.tf
@@ -0,0 +1,29 @@
+provider "azuredevops" {  
+#    organization = "catware"
+    version = ">= 0.0.1"
+}
+
+resource "azuredevops_project" "project" {
+	 project_name                = "bank"
+}
+
+resource "azuredevops_serviceendpoint_github" "github_serviceendpoint" {
+  project_id             = azuredevops_project.project.id
+  service_endpoint_name  = "GitHub Service Connection"
+  github_service_endpoint_pat = var.github_token
+}
+
+resource "azuredevops_build_definition" "build" {
+  project_id      = azuredevops_project.project.id
+  agent_pool_name = "Hosted Ubuntu 1604"
+  name            = "Build"
+  path            = "\\"
+
+  repository {
+    repo_type             = "GitHub"
+    repo_name             = "rroart/bank"
+    branch_name           = "master"
+    yml_path              = "terraform/azure/azure-pipeline.yml"
+    service_connection_id = azuredevops_serviceendpoint_github.github_serviceendpoint.id
+  }
+}
---
+++ terraform/azure/main3.tf
@@ -0,0 +1,47 @@
+# Create an App Service Plan with Linux
+resource "azurerm_app_service_plan" "appserviceplan" {
+  name                = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  reserved = true # Mandatory for Linux plans
+
+  # Define Linux as Host OS
+  kind = "Linux"
+
+  # Choose size
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+
+}
+
+# Create an Azure Web App for Containers in that App Service Plan
+resource "azurerm_app_service" "dockerapp" {
+  name                = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
+
+  # Do not attach Storage by default
+  app_settings = {
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
+
+    /*
+    # Settings for private Container Registires  
+    DOCKER_REGISTRY_SERVER_URL      = ""
+    DOCKER_REGISTRY_SERVER_USERNAME = ""
+    DOCKER_REGISTRY_SERVER_PASSWORD = ""
+    */
+  }
+
+  # Configure Docker Image to load on start
+  site_config {
+    linux_fx_version = "DOCKER|appsvcsample/static-site:latest"
+    always_on        = "true"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
